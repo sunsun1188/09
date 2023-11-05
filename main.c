6211b6599c79cfd7f75fbc1d181d580b997639b5
sunsun1188 <eunsun08110811@gmail.com>
2023-11-05 23:28:06 +0900

    실습2

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define STUDENT   5
+#define STUDENT 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	int grade[STUDENT];
 	int i;
+	printf("5명의 점수를 입력하세요.");
 	
 	for (i=0;i<STUDENT;i++)
-		grade[i]=(i+1)*10;
-	
+		scanf("%d",&grade[i]);
+			
 	for (i=0;i<STUDENT;i++)
 		printf("grade[%d] = %d\n",i,grade[i]);
 	return 0;
