3e7b32bc301340d7aba7b7493d0d93e66e3fa0de
sunsun1188 <eunsun08110811@gmail.com>
2023-11-05 23:36:31 +0900

    실습2-2

--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 int main(int argc, char *argv[]) {
 	
 	int grade[STUDENT];
-	int i;
+	int i,average,sum;
 	printf("5명의 점수를 입력하세요.");
 	
-	for (i=0;i<STUDENT;i++)
+	sum=0;
+	
+	for (i=0;i<STUDENT;i++) {
 		scanf("%d",&grade[i]);
-			
-	for (i=0;i<STUDENT;i++)
-		printf("grade[%d] = %d\n",i,grade[i]);
+		sum += grade[i]; 
+	}
+	average=sum/STUDENT;		
+	printf("성적 평균 : %d\n",average);
 	return 0;
 }
